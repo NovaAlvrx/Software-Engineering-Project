2d59b29f50e8b2fedb7ffa4500dec8ccd5864b28
suzunak <suzunakimura@gmail.com>
2025-09-30 19:43:32 -0400

    Initialize frontend with React

---
+++ skill-share/src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <p className="read-the-docs">
+        Nothing on our page yet!
+      </p>
+    </>
+  )
+}
+
+export default App
