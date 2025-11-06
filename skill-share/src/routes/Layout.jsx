355d75d5e8f3a39a1fc80ef84c9a488f55921280
suzunak <suzunakimura@gmail.com>
2025-11-05 23:00:51 -0500

    Layout for website

---
+++ skill-share/src/routes/Layout.jsx
@@ -0,0 +1,18 @@
+import './Layout.css'
+import NavBar from '../components/navbar/NavBar.jsx';
+import { Outlet } from 'react-router-dom';
+
+function Layout() {
+    console.log("Layout rendered");
+    return (
+        <div className="layout-container">
+            <div className="navbar-container">
+                <NavBar />
+            </div>
+            <div className="main-content">
+                <Outlet />
+            </div>
+        </div>
+    )
+}
+export default Layout;
