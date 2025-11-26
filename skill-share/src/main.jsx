23922ba8e998d06a541db67f099ec1e8f863960c
gerardorivera04 <rivera14gerardo@gmail.com>
2025-11-26 00:32:01 -0500

    Added a route to the Create Page

--- skill-share/src/main.jsx
+++ skill-share/src/main.jsx
@@ -9,6 +9,7 @@ import Profile from './pages/profile-page/Profile.jsx'
 import PostDetail from './pages/PostDetail.jsx'
 import SessionsRev from './pages/SessionsRev.jsx'
 import Explore from './pages/explore-page/Explore.jsx'
+import Create from './pages/create-page/Create.jsx'
 import MockSignUp from './pages/mock-sign-up/MockSignUp.jsx'
 import MockLogin from './pages/mock-login/MockLogin.jsx'
 import {
@@ -26,6 +27,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="/profile/:id" element={<Profile />} />
           <Route path="/profile/:id/post/:post_id" element={<PostDetail />} />
           <Route path="/explore" element={<Explore />} />
+          <Route path="/create" element={<Create />} />
         </Route>
         <Route path="/mock-sign-up" element={<MockSignUp />} />
         <Route path="/mock-login" element={<MockLogin />} />
